57762e1426bd00aac2c8a2fb190a093d8a2937fa
Isabela Müller <isabelamuller1@hotmail.com>
2022-03-19 23:38:01 -0300

    a

---
+++ src/main/java/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bem vindo(a)! Precisamos de alguns dados para o seu cadastro em nossa empresa Farmaco % cia.");
+        System.out.println("Qual seu primeiro nome?"); // 1
+        String nome = sc.next();
+        System.out.println("E seu sobrenome?"); //2
+        String sobrenome = sc.next();
+        System.out.println("Seja bem vindo(a), " + nome + "! Qual é sua idade?"); // 3
+        int idade = sc.nextInt();
+        System.out.println("E qual é o seu gênero? (Responder F para feminino ou M para masculino)"); // 4
+        char letra = sc.next().charAt(0);
+        System.out.println("Qual o seu CPF? (Exemplo: 123.456.789-00)"); // 5
+        String cpf = sc.next();
+        System.out.println("Você possui plano de saúde? Se sim, qual?"); // 6
+        String planoSaude = sc.next();
+        System.out.println("Você é PCD? (Pessoa com deficiência). Responder com sim ou não."); // 7
+        String respostaPCD = sc.next();
+        System.out.println("Você já possui o cartão fidelidade Farmaco & cia?"); // 8
+        String cartao = sc.next();
+        System.out.println("Qual seu telefone? (Com DDD, sem espaço e sem travessão)"); // 9
+        String telefone = sc.next();
+        System.out.println("Como conheceu a Farmaco & cia?"); // 10
+        String conhecer = sc.next();
+        System.out.println("Muito obrigado! Seu cadastro está concluído.");
+
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Sobrenome: " + sobrenome);
+        System.out.println("Nome completo: " + nome + " " + sobrenome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Gênero: " + letra);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Possui plano de saúde? " + planoSaude);
+        System.out.println("É PCD? " + respostaPCD);
+        System.out.println("Possui cartão de fidelidade? " + cartao);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("Como conheceu a empresa? " + conhecer);
+
+    }
+}
+
